03c80fc9d045710d9923deec2c36fdbdb47d409e
Alexander <alexander.wirdemo@gmail.com>
2022-05-13 18:48:32 +0200

    First setup of classes and test

---
+++ src/main/java/PokerHand.java
@@ -0,0 +1,9 @@
+public class PokerHand {
+
+    public static String checkPair(Card[] hand) {
+        for(int index=0; index<hand.length; index++){
+
+        }
+        return "Poker hand";
+    }
+}
