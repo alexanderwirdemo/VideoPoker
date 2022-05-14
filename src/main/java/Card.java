e16e59bd2e56572ae63f414ae97dbf1637c30df0
Alexander <alexander.wirdemo@gmail.com>
2022-05-14 12:33:49 +0200

    Introduced additional variable, faceValue, to Card and evaluated card value stats in separate function

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -2,11 +2,51 @@ public class Card {
     int cardNo; // this variable representents one of the 52 cards and is unique for each card
     Suits suit; // this variable represents the card's suit (HEARTS,CLOVES,DIAMONDS,SPADES)
     int value; // this variable represents the card's value (2-10, Jack, Queen, King, Ace, where 11-Jack, 12-Queen, 13-King and 14-Ace)
+    String faceValue;
 
     public Card(int cardNo, Suits suit, int value) {
         this.cardNo = cardNo;
         this.suit = suit;
         this.value = value;
+        if(value==2){
+            this.faceValue="TWO";
+        }
+        if(value==3){
+            this.faceValue="THREE";
+        }
+        if(value==4){
+            this.faceValue="FOUR";
+        }
+        if(value==5){
+            this.faceValue="FIVE";
+        }
+        if(value==6){
+            this.faceValue="SIX";
+        }
+        if(value==7){
+            this.faceValue="SEVEN";
+        }
+        if(value==8){
+            this.faceValue="EIGHT";
+        }
+        if(value==9){
+            this.faceValue="NINE";
+        }
+        if(value==10){
+            this.faceValue="TEN";
+        }
+        if(value==11){
+            this.faceValue="JACK";
+        }
+        if(value==12){
+            this.faceValue="QUEEN";
+        }
+        if(value==13){
+            this.faceValue="KING";
+        }
+        if(value==14){
+            this.faceValue="ACE";
+        }
     }
 
     public int getValue() {
@@ -17,8 +57,13 @@ public class Card {
         this.value = value;
     }
 
+    public String getFaceValue() {
+        return faceValue;
+    }
+
     @Override
     public String toString() {
+        /*
         String faceValue = "";
         int value = getValue();
         if(value==2){
@@ -59,7 +104,7 @@ public class Card {
         }
         if(value==14){
             faceValue="ACE";
-        }
+        }*/
         return faceValue + " of " +suit;
     }
 }
--- src/main/java/PokerHand.java
+++ src/main/java/PokerHand.java
@@ -1,9 +1,71 @@
-public class PokerHand {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PokerHand implements Comparator<Card>{
 
     public static String checkPair(Card[] hand) {
-        for(int index=0; index<hand.length; index++){
 
+        getHandData(hand);
+
+        Arrays.sort(hand, new Comparator<Card>() {
+            @Override
+            public int compare(Card card1, Card card2) {
+                return Math.max(card1.value, card2.value);
+            }
+        });
+
+
+
+
+
+        for(int index=0; index<hand.length; index++){
+            int firstCardValue = hand[index].value;
+            int cardsOfTheSameValue = 1;
+            for(int index2=index+1; index2<hand.length; index2++){
+                int nextCardValue = hand[index2].value;
+                //System.out.println("Values to compare: "+firstCardValue+" "+nextCardValue);
+                if(firstCardValue==nextCardValue){
+                    //System.out.println("Match!");
+                    cardsOfTheSameValue++;
+                }
+            }
+            System.out.println("Iteration "+(index+1)+": "+cardsOfTheSameValue+" "+firstCardValue);
         }
         return "Poker hand";
     }
+
+    private static void getHandData(Card[] hand) {
+        Map<String, Integer> handValues = new HashMap<>();
+        handValues.put("TWO",0);
+        handValues.put("THREE",0);
+        handValues.put("FOUR",0);
+        handValues.put("FIVE",0);
+        handValues.put("SIX",0);
+        handValues.put("SEVEN",0);
+        handValues.put("EIGHT",0);
+        handValues.put("NINE",0);
+        handValues.put("TEN",0);
+        handValues.put("JACK",0);
+        handValues.put("QUEEN",0);
+        handValues.put("KING",0);
+        handValues.put("ACE",0);
+
+        for(Card card:hand){
+            handValues.put(card.getFaceValue(),handValues.get(card.getFaceValue())+1);
+        }
+
+        for(String key:handValues.keySet()){
+            int value = handValues.get(key);
+            System.out.println(key+" "+value);
+        }
+
+    }
+
+    @Override
+    public int compare(Card card1, Card card2) {
+        return Math.max(card1.value, card2.value);
+    }
+
 }
