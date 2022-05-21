7bbdb807187236997a910df9ff539b93dd743770
Alexander <alexander.wirdemo@gmail.com>
2022-05-21 16:28:03 +0200

    Introduced second test to test non-two pairs

--- src/test/java/PokerHandsEvaluator.java
+++ src/test/java/PokerHandsEvaluator.java
@@ -1,39 +1,47 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PokerHandsEvaluator {
 
     @Test
     public void onePair() {
         Card firstCard = new Card(2, Suits.CLOVES, 2);
-        Card seconCard = new Card(3, Suits.CLOVES, 3);
+        Card secondCard = new Card(3, Suits.CLOVES, 3);
         Card thirdCard = new Card(4, Suits.CLOVES, 4);
         Card forthCard = new Card(15, Suits.DIAMONDS, 2);
         Card fifthCard = new Card(26, Suits.DIAMONDS, 13);
+        Card oddCard = new Card(25, Suits.DIAMONDS, 12);
         Card[] onePairHand = new Card[5];
         onePairHand[0]=firstCard;
-        onePairHand[1]=seconCard;
+        onePairHand[1]=secondCard;
         onePairHand[2]=thirdCard;
         onePairHand[3]=forthCard;
         onePairHand[4]=fifthCard;
         assertEquals("One pair", PokerHand.checkHand(onePairHand));
+        // Non-one pair hand introduced
+        onePairHand[3]=oddCard;
+        assertNotEquals("One pair", PokerHand.checkHand(onePairHand));
     }
 
     @Test
-    public void twoPair() {
+    public void twoPairs() {
         Card firstCard = new Card(2, Suits.CLOVES, 2);
-        Card seconCard = new Card(3, Suits.CLOVES, 3);
+        Card secondCard = new Card(3, Suits.CLOVES, 3);
         Card thirdCard = new Card(4, Suits.CLOVES, 4);
         Card forthCard = new Card(15, Suits.DIAMONDS, 2);
         Card fifthCard = new Card(16, Suits.DIAMONDS, 3);
+        Card oddCard = new Card(25, Suits.DIAMONDS, 12);
         Card[] twoPairsHand = new Card[5];
         twoPairsHand[0]=firstCard;
-        twoPairsHand[1]=seconCard;
+        twoPairsHand[1]=secondCard;
         twoPairsHand[2]=thirdCard;
         twoPairsHand[3]=forthCard;
         twoPairsHand[4]=fifthCard;
-        assertEquals("Two pair", PokerHand.checkHand(twoPairsHand));
+        assertEquals("Two pairs", PokerHand.checkHand(twoPairsHand));
+        twoPairsHand[3]=oddCard;
+        assertNotEquals("Two pairs", PokerHand.checkHand(twoPairsHand));
     }
 
 
